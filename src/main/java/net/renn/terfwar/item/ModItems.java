f493c57621a1ce1419aa1b292d577ede541e037e
Renn <renzenmode@gmail.com>
2025-08-08 14:41:08 -0400

    Added eraser item, but the texture is bugged.

---
+++ src/main/java/net/renn/terfwar/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.renn.terfwar.item;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.renn.terfwar.TerfWar;
+
+public class ModItems {
+
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(TerfWar.MOD_ID);
+
+    public static final DeferredItem<Item> ERASER = ITEMS.register("eraser",
+            ()-> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
